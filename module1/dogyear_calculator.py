ea592731cc50cbeca9a6f58cfc9ec550b457ea12
Paul Fleming <pfleming@pfleming-test10.internal.salesforce.com>
2019-05-26 19:36:46 +0100

    Added dogyear_calculator.py

---
+++ module1/dogyear_calculator.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+age = input("How old are you? ")
+age = int(age)
+dog_years = age * 9
+
+print("If you were a dog, you'd be ", dog_years, "!!")
+print('''
+
+    o____
+     ||||
+''')
